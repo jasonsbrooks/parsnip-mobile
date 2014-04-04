70ea79db61f230c06b3a6dfc39ff075311e9ac0c
Charles Jin <charlesjin@snorlax.wireless.yale.internal>
2014-04-03 21:18:03 -0400

    added sample view for example's sake

--- Heaped/HeapTriangulate.h
+++ Heaped/HeapTriangulate.h
@@ -5,21 +5,21 @@
 //  Created by Michael Zhao on 4/3/14.
 //  Copyright (c) 2014 Michael Zhao. All rights reserved.
 //
-
-#import <Foundation/Foundation.h>
-
-@interface HeapTriangulate : NSObject
-{
-    int x0;
-    int x1;
-    int x2;
-    
-    int y0;
-    int y1;
-    int y2;
-}
-
--(NSArray *)triangulate:(NSArray *)dist;
--(float)angle:(float)c a:(float)a b:(float)b;
-
-@end
+//
+//#import <Foundation/Foundation.h>
+//
+//@interface HeapTriangulate : NSObject
+//{
+//    int x0;
+//    int x1;
+//    int x2;
+//    
+//    int y0;
+//    int y1;
+//    int y2;
+//}
+//
+//-(NSArray *)triangulate:(NSArray *)dist;
+//-(float)angle:(float)c a:(float)a b:(float)b;
+//
+//@end
